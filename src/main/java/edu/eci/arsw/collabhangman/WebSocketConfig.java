933dd0f7becb20eb0a145f9786d072c06dfac402
Laura Ramos <lauramilena.rbe@gmail.com>
2017-11-04 01:40:35 -0500

    correccion ip

--- src/main/java/edu/eci/arsw/collabhangman/WebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabhangman/WebSocketConfig.java
@@ -33,7 +33,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         //config.enableSimpleBroker("/topic");
-        config.enableStompBrokerRelay("/topic/").setRelayHost("192.168.56.1").setRelayPort(61613);
+        config.enableStompBrokerRelay("/topic/").setRelayHost("192.168.56.102").setRelayPort(61613);
         config.setApplicationDestinationPrefixes("/app");
     }
     /*
